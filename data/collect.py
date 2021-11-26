da4fdba8fca58f3c1d85b516388b2c20789a624e
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2021-11-26 13:16:13 -0500

    finish collection script

--- data/collect.py
+++ data/collect.py
@@ -1,34 +1,44 @@
 import requests
-import json
+from redditcleaner import clean
 
 BASE_URL = 'http://api.reddit.com'
+USER_AGENT = {'User-agent': 'two-solitudes-project'}
 
 def save_comments_to_file(subreddit, min_amount, post_sort, post_timeframe, comment_sort, 
                     filename=None, max_comments_per_post=20):
     if not filename:
-        filename = f'{subreddit}-{post_sort}_posts-{min_amount}_{comment_sort}_comments'
+        filename = f'{subreddit}-{post_sort}_posts-{min_amount}_{comment_sort}_comments.txt'
 
-    # https://www.jcchouinard.com/reddit-api-without-api-credentials/
-    # https://www.reddit.com/dev/api/#GET_hot
-
-    post_fullnames = []
+    post_ids = []
     comment_count = 0
     while comment_count < min_amount:
-        # TODO: make sure the after word when there's nothing in post_fullnames
-        next_100_posts = requests.get(f'{BASE_URL}/r/{subreddit}/{post_sort} \
-                                        ?after={post_fullnames[-1]} \
-                                        &timeframe={post_timeframe} \
-                                        &limit=100') \
-                                .json()['data']['children']
-        for post in next_100_posts:
-            post_fullnames.append(f'{post["kind"]}_{post["id"]}')
+        last_post = f't3_{post_ids[-1]}' if len(post_ids) != 0 else ''
+        next_100_posts = requests.get(f'{BASE_URL}/r/{subreddit}/{post_sort}',
+                                        params={'after': last_post,
+                                                'timeframe': post_timeframe,
+                                                'limit': 100},
+                                        headers=USER_AGENT)\
+                                    .json()['data']['children']
+        for _post in next_100_posts:
+            post = _post['data']
+            post_ids.append(post["id"])
             comment_count += post['num_comments'] if post['num_comments'] < max_comments_per_post \
                                             else max_comments_per_post
 
-    # make API calls to post_fullnames
-    # https://www.reddit.com/dev/api/#GET_comments_{article}
-    # save to a dataframe and then to_csv() that? or just use vanilla Python?
-
+    # TODO: this does not handle replies!!! If I'm not going to do that, set the depth to 1 to speed things up.
+    comments = []
+    for post_id in post_ids:
+        post_comments = requests.get(f'{BASE_URL}/r/{subreddit}/comments/{post_id}',
+                                    params={'limit': max_comments_per_post,
+                                            'sort': comment_sort},
+                                    headers=USER_AGENT) \
+                                .json()
+        post_comments = [comment['data']['children'][0] for comment in post_comments
+                        if len(comment['data']['children']) > 0]
+        comments.extend([clean(comment['data']['body']) for comment in post_comments \
+                                    if comment['kind'] == 't1'])
+    with open(filename, 'w') as fp:
+        fp.write('\n'.join(comments))
+    
 if __name__ == "__main__":
-    # TODO: call function appropriately
-    pass
+    save_comments_to_file('Quebec', 100, 'new', 'all', 'top', max_comments_per_post=3)
