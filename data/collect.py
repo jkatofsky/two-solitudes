909fe301e920d4c82310a25910c1bbcdcc95e952
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2021-11-27 00:39:39 -0500

    before bed commit

--- data/collect.py
+++ data/collect.py
@@ -1,13 +1,15 @@
 import requests
 from redditcleaner import clean
+from googletrans import Translator
 
 BASE_URL = 'http://api.reddit.com'
 USER_AGENT = {'User-agent': 'two-solitudes-project'}
 
-def save_comments_to_file(subreddit, min_amount, post_sort, comment_sort, 
-                    filename=None, max_comments_per_post=20):
+# TODO: improve verbosity
+def save_comments_to_file(subreddit, min_amount, post_sort, comment_sort, timeframe='all',
+                    filename=None, max_comments_per_post=20, save_english_translation=False):
     if not filename:
-        filename = f'{subreddit}-{post_sort}_posts-{min_amount}_{comment_sort}_comments.txt'
+        filename = f'data/{subreddit}-{post_sort}_posts-{min_amount}_{comment_sort}_comments.txt'
 
     post_ids = []
     comment_count = 0
@@ -15,6 +17,8 @@ def save_comments_to_file(subreddit, min_amount, post_sort, comment_sort,
         last_post = f't3_{post_ids[-1]}' if len(post_ids) != 0 else ''
         next_100_posts = requests.get(f'{BASE_URL}/r/{subreddit}/{post_sort}',
                                         params={'after': last_post,
+                                                'timeframe': timeframe,
+                                                'count': len(post_ids),
                                                 'limit': 100},
                                         headers=USER_AGENT)\
                                     .json()['data']['children']
@@ -27,7 +31,7 @@ def save_comments_to_file(subreddit, min_amount, post_sort, comment_sort,
         if len(next_100_posts) < 100:
             break
 
-    # TODO: handle replies!!!
+    # TODO: handle replies!!! This is crucial to get enough comments!
     comments = []
     for post_id in post_ids:
         post_comments = requests.get(f'{BASE_URL}/r/{subreddit}/comments/{post_id}',
@@ -41,9 +45,17 @@ def save_comments_to_file(subreddit, min_amount, post_sort, comment_sort,
             comments.extend([clean(comment['data']['body']) for comment in comment_list \
                                     if comment['kind'] == 't1'])
         print(f'Got {len(comments)} comments')
+
     with open(filename, 'w') as fp:
         fp.write('\n'.join(comments))
 
-# TODO: give this script command line args?
+    if save_english_translation:
+        translator = Translator()
+        translated_comments = translator.translate(comments, dest='en')
+        with open(f'EN-{filename}', 'w') as fp:
+            fp.write('\n'.join((translation.text for translation in translated_comments)))
+
+# TODO: time permitting, give this script command line args
 if __name__ == "__main__":
-    save_comments_to_file('Quebec', 1000, 'new', 'top')
+    # save_comments_to_file('Quebec', 10000, 'top', 'top', save_english_translation=True)
+    save_comments_to_file('Canada', 10000, 'top', 'top')
