02e11b458bb9ba3de04431823b96c6b766bb3312
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2021-11-29 22:51:43 -0500

    more textgen

--- nlp/frequency.py
+++ nlp/frequency.py
@@ -5,6 +5,7 @@ from nltk.stem import WordNetLemmatizer
 from nltk.probability import FreqDist
 from nltk import ngrams
 import pandas as pd
+from wordcloud import WordCloud
 
 from utils import get_data
 
@@ -30,18 +31,30 @@ def word_frequency(dataset, top_n=500):
 
     return nouns_freq, bigrams_freq
 
+def save_word_cloud(freq_file_path):
+    df = pd.read_csv(freq_file_path)
+    frequencies_dict = dict(zip(df.word, df.frequency))
+    # 333f50
+    wc = WordCloud(background_color="white", max_words=200, height=900, width=1600)
+    wc.generate_from_frequencies(frequencies_dict)
+    fn, _ = freq_file_path.split('.')
+    wc.to_file(f'{fn}.png')
+
 if __name__ == "__main__":
     # QC_data = get_data('QC', exclude=['montreal', 'quebeccity'])
     # QC_freq_nouns, QC_freq_bigrams = word_frequency(QC_data)
     # QC_freq_nouns.to_csv('quebec-no-cities-freq-nouns.csv', index=False)
     # QC_freq_bigrams.to_csv('quebec-no-cities-freq-bigrams.csv', index=False)
 
-    ROC_data = get_data('CA')
-    ROC_freq_nouns, ROC_freq_bigrams = word_frequency(ROC_data)
-    ROC_freq_nouns.to_csv('ROC-freq-nouns.csv', index=False)
-    ROC_freq_bigrams.to_csv('ROC-freq-bigrams.csv', index=False)
+    # ROC_data = get_data('CA')
+    # ROC_freq_nouns, ROC_freq_bigrams = word_frequency(ROC_data)
+    # ROC_freq_nouns.to_csv('ROC-freq-nouns.csv', index=False)
+    # ROC_freq_bigrams.to_csv('ROC-freq-bigrams.csv', index=False)
     # ROC_freq = word_frequency(get_data('ROC'))
     # print(ROC_freq)
     # fig, axes = plt.subplots(3, 1 ,figsize=(8,20))
     # sns.barplot(ax=axes[0], x='frequency', y='word', data=QC_freq.head(30))
     # sns.barplot(ax=axes[0], x='frequency', y='word', data=ROC_freq.head(30))
+
+    save_word_cloud('frequency/QC-no-cities-freq-nouns.csv')
+    save_word_cloud('frequency/ROC-freq-nouns.csv')
--- nlp/generation.py
+++ nlp/generation.py
@@ -16,4 +16,18 @@ def train_model(file, output_dir):
 
 if __name__ == "__main__":
     # train_model('../data/data/QC/merged-CLEAN.txt', 'models/QC')
-    train_model('../data/data/CA/merged-CLEAN.txt', 'models/CA')
+    # train_model('../data/data/CA/merged-CLEAN.txt', 'models/CA')
+    QC_model = aitextgen(model_folder='models/QC', \
+        tokenizer_file='models/QC/aitextgen.tokenizer.json')
+    CA_model = aitextgen(model_folder='models/CA', \
+        tokenizer_file='models/CA/aitextgen.tokenizer.json')
+    prompts = ['quebec is', 'canada is']
+    # QC_model.generate_to_file(n=100, destination_path='models/QC/samples.txt')
+    # CA_model.generate_to_file(n=100, destination_path='models/CA/samples.txt')
+    for prompt in prompts:
+        print('QUEBEC:')
+        QC_model.generate(n=20, prompt=prompt)
+        print('CANADA:')
+        QC_model.generate(n=20, prompt=prompt)
+    # print(QC_outputs)
+    # print(CA_outputs)
